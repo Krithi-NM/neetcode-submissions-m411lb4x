5c70666a01f78c2cdbcec7da5aefd4936b010899
Krithi-NM <kruthinm1210@gmail.com>
2026-04-06 19:35:36 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int robLinear(vector<int>&nums,int l,int r){
+        int prev2=0;
+        int prev1=nums[l];
+        for(int i=l+1;i<=r;i++){
+            int pick=nums[i]+prev2;
+            int notpick=prev1;
+            int curr=max(pick,notpick);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+    
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int case1=robLinear(nums,0,n-2);
+        int case2=robLinear(nums,1,n-1);
+        return max(case1,case2);
+    }
+};
